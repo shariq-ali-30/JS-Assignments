04512125b83143d9a33b16dad5b2525d49524431
shariq-ali-30 <shariq3072007@gmail.com>
2026-02-19 00:57:20 +0500

    File Upload

---
+++ sript.js
@@ -0,0 +1,82 @@
+// CHAPTER 1: ALERTS
+
+// Question # 1
+// alert("Hello, Shariq!")
+
+// Question # 2
+// alert("Error! Please enter a valid password")
+
+// Question # 3
+// alert("Hello  and Welcome! \nHave a wonderful day ahead.")
+
+// Question # 4
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+// Question # 5
+// alert("Hello... I can run JS through my web brower's console") "Ye code console me likha tha, kaam kar raha hai 👍🏻"
+
+// Question # 6
+
+// Question # 7
+// d. Body (after your page’s HTML)
+
+// CHAPTER 2: VARIABLES FOR STRINGS
+
+// Question # 1
+// var username
+
+// Question # 2
+// var myName = "Shariq Ali"
+
+// Question # 3
+// var message = "Hello World"
+// alert(message)
+
+// Question # 4
+// var studentName = "Shariq Ali"
+// var studentAge = "18 years old"
+// var course = "Web and Mobile App Development"
+// alert(studentName)
+// alert(studentAge)
+// alert(course)
+
+// Question # 5
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(pizza)
+
+// Question # 6
+// var email = "shariq3072007@gmail.com"
+// alert("My email address is " + email)
+
+// Question # 7
+// var book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the Book " + book)
+
+// Question # 8
+// document.write("Yah! I can write HTML content through JavaScript")
+
+// Question # 9
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design)
+// document.write(design)
+
+// CHAPTER 3: VARIABLES FOR NUMBERS
+
+// Question # 1
+// var age = 18
+// alert("I am " + age + " years old")
+
+// Question # 2
+// var visitCount = 14
+// alert("You have visited this site " + visitCount + " times")
+
+// Question # 3
+// var birthYear = 2007
+// document.write("My birth year is " + birthYear + "<br>" + "Data type of my declared variable is number")
+
+// Question # 4
+// var visitorsName = "Shariq Ali"
+// var prodrctTitle = "T-shirt(s)"
+// var quantity = 5
+// document.write(visitorsName + " ordered " + quantity + " " + prodrctTitle + " on XYZ Clothing store.")
