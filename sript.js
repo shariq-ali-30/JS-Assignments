c732dbcc6f277c0d65a07b967344a4a9f148afac
shariq-ali-30 <shariq3072007@gmail.com>
2026-02-20 20:06:56 +0500

    Chapter 4 & 5 Assignments added

--- sript.js
+++ sript.js
@@ -80,3 +80,83 @@
 // var prodrctTitle = "T-shirt(s)"
 // var quantity = 5
 // document.write(visitorsName + " ordered " + quantity + " " + prodrctTitle + " on XYZ Clothing store.")
+
+// CHAPTER 4: VARIABLE NAMES: LEGAL & ILLEGAL
+
+// Question # 1
+// var name, age, course
+
+// Quetion # 2
+// Legal variable names
+// var username
+// var userName
+// var user_name
+// var UserName
+// var USER_NAME
+
+// Illegal variable names
+// var 1username
+// var user-name
+// var user name
+// var user@name
+// var user.name
+
+// Question # 3
+// document.write("Rules for naming JS variables" + "<br>" + "<br>")
+// document.write("Variable names can only contain letters, numbers, $ and _. For example: $my_1stVariable" + "<br>")
+// document.write("Variables must begin with a letter, $ or _. For example: $name, _name or name" + "<br>")
+// document.write("Variable names are sensitive " + "<br>")
+// document.write("Variable names should not be JavaScript keywords" + "<br>")
+
+// CHAPTER 5: MATH EXPRESSIONS
+
+// Question # 1
+// var num1 = 3
+// var num2 = 5
+// var sum = num1 + num2
+// document.write("Sum of " + num1 + " and " + num2 + " is " + sum)
+
+// Question # 2
+// var num1 = 10
+// var num2 = 5
+// var sub = num1 - num2
+// var mul = num1 * num2
+// var div = num1 / num2
+// var mod = num1 % num2
+// document.write("Subtraction of " + num1 + " and " + num2 + " is " + sub + "<br>")
+// document.write("Multiplication of " + num1 + " and " + num2 + " is " + mul + "<br>")
+// document.write("Division of " + num1 + " and " + num2 + " is " + div + "<br>")
+// document.write("Modulus of " + num1 + " and " + num2 + " is " + mod)
+
+// Question # 3
+// var num
+// document.write("Value after variable declaration is: " + num + "<br>")
+// num = 5
+// document.write("Initial value: " + num + "<br>")
+// num++
+// document.write("Value after increment is: " + num + "<br>")
+// num = num + 7
+// document.write("Value after addition is: " + num + "<br>")
+// num--
+// document.write("Value after decrement is: " + num + "<br>")
+// num = num % 3
+// document.write("The remainder is: " + num)
+
+// Question # 4
+// var ticketPrice = 600
+// var totalCost = ticketPrice * 5
+// document.write("Total cost to buy 5 tickets to a movie is " + totalCost + "PKR")
+
+// Question # 5
+// var num = 4
+// document.write("Table of " + num + "<br><br>")
+// document.write(num + " x 1 = " + num * 1 + "<br>")
+// document.write(num + " x 2 = " + num * 2 + "<br>")
+// document.write(num + " x 3 = " + num * 3 + "<br>")
+// document.write(num + " x 4 = " + num * 4 + "<br>")
+// document.write(num + " x 5 = " + num * 5 + "<br>")
+// document.write(num + " x 6 = " + num * 6 + "<br>")
+// document.write(num + " x 7 = " + num * 7 + "<br>")
+// document.write(num + " x 8 = " + num * 8 + "<br>")
+// document.write(num + " x 9 = " + num * 9 + "<br>")
+// document.write(num + " x 10 = " + num * 10)
